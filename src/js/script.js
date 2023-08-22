f1f2fcaeb72d1d3357f63715111e1939049e1b8a
Vitalii Hrytsenko <vitalik.gricenko77@gmail.com>
2023-08-22 22:37:08 +0300

    add new blocks (faq, contact, footer), add typing effect

---
+++ src/js/script.js
@@ -0,0 +1,45 @@
+let accordionNav = document.querySelectorAll('.faq-accordion-nav'),
+    accordionBody = document.querySelectorAll('.faq-accordion-body'),
+    accordionImage = document.querySelectorAll('.faq-accordion-image svg path');
+
+
+
+
+function openAccordion(i) {
+  accordionImage[i].setAttribute('d', 'M8 12H16M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12ZM8 12H16M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12Z')
+  accordionBody[i].classList.add('faq-accordion-body--active');
+}
+function closeAccordion(i) {
+  accordionBody[i].classList.remove('faq-accordion-body--active');
+  accordionImage[i].setAttribute('d', 'M12 8V16M8 12H16M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12Z')
+}
+
+openAccordion(0);
+
+accordionNav.forEach((item, i) => {
+  item.addEventListener('click', () => {
+    if(accordionBody[i].classList.contains('faq-accordion-body--active')) {
+      closeAccordion(i)
+    } else {
+      openAccordion(i)
+      for(let j = 0; j < accordionBody.length; j++) {
+        if(j !== i) {
+          accordionBody[j].classList.remove('faq-accordion-body--active')
+        }
+        
+      }
+    }
+    
+  })
+})
+
+
+
+var typed = new Typed('.typing', {
+  strings: ['web', 'app', 'SaaS'],
+  loop: true,
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1900,
+  showCursor: false,
+});
